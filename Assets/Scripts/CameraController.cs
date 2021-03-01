fe793638e3d843cbc387784b736048dcd339f258
and49 <and49@aber.ac.uk>
2021-03-01 14:31:16 +0000

    First
    Prototype

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    //follow point is the point at which the camera will follow (in this case it's our character sprite).
+    public Transform followPoint;
+
+    //offset will keep the camera in front of the sprite.
+    public Vector3 offset;
+
+    //For our Lerp, this will tell us how much we want our camera movement smoothed by.
+    [Range(2,10)] public float smoothingRate;
+
+    private void FixedUpdate()
+    {
+        Follow();        
+    }
+
+
+    void Follow()
+    {
+        Vector3 targetPosition = followPoint.position + offset;
+
+        /** We lerp from our target position, to the new position with smoothing. 
+         * We use fixedDeltaTime here to make the rate the same on every machine, reguardless of the fps. **/
+        Vector3 positionSmoothing = Vector3.Lerp(transform.position, targetPosition, smoothingRate*Time.fixedDeltaTime);
+
+        transform.position = positionSmoothing;
+    }
+
+}
---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+[Range(0, .3f)] public float MovementSmoothing = .05f ; // How much to smooth out the movement
+public float JumpForce = 400f ; // Amount of force added when the player jumps .
+public float MoveForce = 10f ; // Amount of force added when the player jumps .
+public LayerMask Ground;// A mask determining what is ground to the character
+private Rigidbody2D rb;
+private Animator myAnimator;
+private BoxCollider2D bCollider;
+private Vector3 currentVelocity = Vector3.zero;
+private int jumpCounter = 0;
+private float jumpForce = 10.0f;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myAnimator = GetComponent<Animator>();
+        bCollider = GetComponent <BoxCollider2D>(); 
+        rb = GetComponent<Rigidbody2D>();
+        
+      
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        float move = Input.GetAxis("Horizontal");        
+
+        // Move the character by finding the target velocity
+        Vector3 targetVelocity = new Vector2(move * MoveForce, rb.velocity.y);
+
+        // And then smoothing it out and applying it to the character
+        rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref currentVelocity,
+        MovementSmoothing);
+
+        // This will flip the character to face left when walking left and right when turning right. 
+        if (move < 0) {
+            transform.localScale = new Vector2(-1, 1);
+        } else
+        {
+            transform.localScale = new Vector2(1, 1);
+        }
+
+        // Jump controls
+
+        if ((Input.GetButtonDown("Jump")) && jumpCounter < 1)
+        {
+            jump();
+        }      
+
+        if (bCollider.IsTouchingLayers(Ground))
+        {
+            resetJumpCounter();
+        }
+
+
+
+    }
+    void jump()
+    {
+        myAnimator.SetTrigger("Jump");
+        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        jumpCounter++;
+    }
+
+    void resetJumpCounter()
+    {
+        jumpCounter = 0;
+    }
+
+}
